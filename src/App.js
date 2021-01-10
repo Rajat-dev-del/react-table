acb8f085007144da6c783ad14eef4f05c1452c6f
rajat <rajat.srivastava@hestabit.in>
2021-01-10 17:44:21 +0530

    react project build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/style.css';
+import About from './components/About.js';
+import Contact from './components/Contact.js';
+import Home from './components/Home.js';
+import Users from './components/Users.js';
+
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Router>
+       <div>
+         <Route exact path ="/" component ={Home} />
+         <Route exact path ="/About" component ={About} />
+         <Route exact path ="/Contact" component ={Contact} />
+         <Route exact path ="/Users" component ={Users} />
+
+       </div>
+       </Router>
     </div>
   );
 }
